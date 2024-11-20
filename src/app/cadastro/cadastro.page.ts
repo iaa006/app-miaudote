d841bbefc7d723af981301369c17b8df8fb8be06
nunesemi <emilianunescabral@gmail.com>
2024-11-20 19:26:33 -0300

    feat: finalização da page de cadastro

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,15 +7,47 @@ import { Router } from '@angular/router';
   styleUrls: ['./cadastro.page.scss'],
 })
 export class CadastroPage {
+  etapa = 1;
   cadastroForm: FormGroup;
+  enderecoForm: FormGroup;
+  usuarioForm: FormGroup;
+  bancarioForm: FormGroup;
+  estados: string[] = [
+    'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal',
+    'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul',
+    'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí',
+    'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia',
+    'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins',
+  ];
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  constructor(private formBuilder: FormBuilder) {
     this.cadastroForm = this.formBuilder.group({
       nome: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      telefone: ['', [Validators.required, Validators.pattern(/^\(\d{2}\) \d{4,5}-\d{4}$/)]],
+      telefone: ['', Validators.required],
       tipoCadastro: ['', Validators.required],
-      documento: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      cpfCnpj: ['', Validators.required],
+    });
+
+    this.enderecoForm = this.formBuilder.group({
+      estado: ['', Validators.required],
+      cidade: ['', Validators.required],
+      logradouro: ['', Validators.required],
+      numero: ['', Validators.required],
+      complemento: [''],
+    });
+
+    this.usuarioForm = this.formBuilder.group({
+      usuario: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmarSenha: ['', Validators.required],
+    }, { validator: this.validarSenhas });
+
+    this.bancarioForm = this.formBuilder.group({
+      agencia: [''],
+      conta: [''],
+      responsavel: [''],
+      chavePix: ['']
     });
   }
 
@@ -24,9 +55,47 @@ export class CadastroPage {
     return this.cadastroForm.controls;
   }
 
+  get formControlsEndereco() {
+    return this.enderecoForm.controls;
+  }
+
+  validarSenhas(formGroup: FormGroup) {
+    const senha = formGroup.get('senha')?.value;
+    const confirmarSenha = formGroup.get('confirmarSenha')?.value;
+    return senha === confirmarSenha ? null : { senhasNaoConferem: true };
+  }
+
+  senhaVisivel = false;
+
+  toggleSenhaVisivel() {
+    this.senhaVisivel = !this.senhaVisivel;
+  }
+
   proximaEtapa() {
-    if (this.cadastroForm.valid) {
-      this.router.navigate(['/cadastro-2']);
+    if (this.etapa === 1 && this.cadastroForm.valid) {
+      this.etapa++;
+    } else if (this.etapa === 2 && this.enderecoForm.valid) {
+      this.etapa++;
+    } else if (this.etapa === 3 && this.usuarioForm.valid) {
+      this.etapa++;
     }
+  }
+
+  voltarEtapa() {
+    if (this.etapa > 1) {
+      this.etapa--;
+    }
+  }
+
+  finalizarCadastro() {
+    const cadastroCompleto = {
+      ...this.cadastroForm.value,
+      ...this.enderecoForm.value,
+      ...this.usuarioForm.value,
+      dadosBancarios: this.bancarioForm.value,
+    };
+  
+    console.log('Cadastro finalizado:', cadastroCompleto);
+    alert('Cadastro concluído com sucesso!');
   }
 }
