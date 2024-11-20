ca018ae3edd375569bb2dc71490a4ca3d580cd88
iaa005 <iagoaa0506@gmail.com>
2024-11-20 11:37:29 -0300

    Feat: Listar usuarios e filtro de usuarios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,8 +9,9 @@ import { OverlayEventDetail } from '@ionic/core/components';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit{
+  public verAnimaisOuDoadores : boolean = true; // True = ver animal - False = ver doadores
   @ViewChild(IonModal) modal!: IonModal;
-  public animais: any[] = []
+  public dadosListagem: any[] = []
   public message: string = '';
   estados: string[] = [
     'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal',
@@ -27,7 +28,7 @@ export class HomePage implements OnInit{
   tamanhoSelecionado: string = '';
   idadeSelecionada: string = '';
   situacaoSelecionada: string = '';
-
+  tipoDoadorSelecionado: string = '';
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
@@ -35,7 +36,11 @@ export class HomePage implements OnInit{
 
   confirm() {
         this.modal.dismiss(null, 'confirm');
-        this.filtrarAnimais();
+        if (this.verAnimaisOuDoadores){
+          this.filtrarAnimais();
+        }else{
+          this.filtrarDoadores();
+        }
   }
 
   onWillDismiss(event: Event) {
@@ -53,16 +58,73 @@ export class HomePage implements OnInit{
   }
   getAnimais(){
     this.apiServices.getAnimais().subscribe((data: any) =>{
-      this.animais = data;
+      this.dadosListagem = data;
+      console.log(data)
+    })
+  }
+
+  getDoadores(){
+    this.apiServices.getDoadores().subscribe((data: any) =>{
+      this.dadosListagem = data;
       console.log(data)
     })
   }
 
   filtrarAnimais(){
+    const filtros = [this.sexoSelecionado, this.especieSelecionada, this.estadoSelecionado, this.cidade, this.tamanhoSelecionado, this.idadeSelecionada, this.situacaoSelecionada]
+
+      if (this.sexoSelecionado === 'Todos'){
+        this.sexoSelecionado = ''
+      }
+      if (this.especieSelecionada === 'Todos'){
+        this.especieSelecionada = ''
+      }
+      if (this.tamanhoSelecionado === 'Todos'){
+        this.tamanhoSelecionado = ''
+      }
+      if (this.idadeSelecionada === 'Todos'){
+        this.idadeSelecionada = ''
+      }
+      if (this.situacaoSelecionada === 'Todos'){
+        this.situacaoSelecionada = ''
+      }
+      if (this.situacaoSelecionada === 'Adotado'){
+        this.situacaoSelecionada = 'indisponivel'
+      }
+      if (this.situacaoSelecionada === 'Disponível'){
+        this.situacaoSelecionada = 'disponivel'
+      }
+
+    console.log(filtros)
     this.apiServices.getAnimaisFiltrados(this.sexoSelecionado, this.especieSelecionada, this.estadoSelecionado, this.cidade, this.tamanhoSelecionado, this.idadeSelecionada, this.situacaoSelecionada).subscribe((data: any) =>{
-      this.animais = data;
+      this.dadosListagem = data;
       console.log(data)
     })
+  }
+
+  filtrarDoadores(){
+    if(this.tipoDoadorSelecionado === "Todos"){
+      this.tipoDoadorSelecionado = ''
+    }
+    if(this.tipoDoadorSelecionado === "Pessoa Comum"){
+      this.tipoDoadorSelecionado = 'PF'
+    }
+    if(this.tipoDoadorSelecionado === "Instituição"){
+      this.tipoDoadorSelecionado = 'PJ'
+    }
+    this.apiServices.getDoadoresFiltrados(this.estadoSelecionado, this.cidade, this.tipoDoadorSelecionado).subscribe((data: any) =>{
+      this.dadosListagem = data;
+      console.log(data)
+    })
+  }
+
+  verAnimais(){
+    this.verAnimaisOuDoadores = true;
+    this.getAnimais();
+  }
+  verDoador(){
+    this.verAnimaisOuDoadores = false;
+    this.getDoadores();
   }
 }
 
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,12 +11,18 @@ export class ApiService {
   getAnimais(){
     return this.http.get("http://127.0.0.1:8000/api/animais/")
   }
-  getAnimaisFiltrados(sexo:string, especie:string, estado:string, cidade:string,
-    tamanho:string,
-    idade:string,
-    situacao:string,){
+  getDoadores(){
+    return this.http.get("http://127.0.0.1:8000/api/usuarios/")
+  }
+  getAnimaisFiltrados(sexo:string, especie:string, estado:string, cidade:string, tamanho:string, idade:string, situacao:string,){
     //http://127.0.0.1:8000/api/animais/?sexo=M&especie=gato&idade=filhote&status=disponivel
 
     return this.http.get(`http://127.0.0.1:8000/api/animais/?sexo=${sexo}&especie=${especie}&idade=${idade}&status=${situacao}`)
+  }
+  getDoadoresFiltrados(estado:string, cidade:string, tipo: string){
+    //http://127.0.0.1:8000/api/animais/?sexo=M&especie=gato&idade=filhote&status=disponivel
+    
+
+    return this.http.get(`http://127.0.0.1:8000/api/usuarios/?tipo=${tipo}&cidade=${cidade}&estado=${estado}`)
   }
 }
